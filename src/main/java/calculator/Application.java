cdbd057dd1d1ca07a08fb1f7ecdc655f2a5a23dc
juhyun <hanaheart030@gmail.com>
2025-10-20 20:38:48 +0900

    feat(io): read user input with Console.readLine()

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,16 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 1. 사용자에게 입력 요청
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+
+        // 2. 문자열 입력 받기
+        String input = Console.readLine();
+
+        // 다음 단계에서 input을 계산기로 넘길 예정
+        System.out.println("입력값: " + input);
     }
 }
